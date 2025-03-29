6b9d02e52def6ce7187e5419a0e80e705111e8ce
Victor Nieto <nietopalaciosvictor@gmail.com>
2025-03-29 11:04:23 +0100

    Spring Initializr

---
+++ src/main/java/es/upm/dit/isst/isstgrupo07flores/Isstgrupo07floresApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.isstgrupo07flores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isstgrupo07floresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isstgrupo07floresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/upm/dit/isst/isstgrupo07flores/Isstgrupo07floresApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.isstgrupo07flores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isstgrupo07floresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
