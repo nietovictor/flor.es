8e6a5b025df9badfd6651c43b179455380920a03
Victor Nieto <nietopalaciosvictor@gmail.com>
2025-05-12 13:50:06 +0200

    Mensaje no verificado para floricultor  y ultimos retoques

--- src/main/java/es/upm/dit/isst/isstgrupo07flores/controller/FlorEsController.java
+++ src/main/java/es/upm/dit/isst/isstgrupo07flores/controller/FlorEsController.java
@@ -1,5 +1,6 @@
 package es.upm.dit.isst.isstgrupo07flores.controller;
 
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.Authentication;
@@ -7,9 +8,16 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import es.upm.dit.isst.isstgrupo07flores.repository.FloricultorRepository;
+
+import es.upm.dit.isst.isstgrupo07flores.model.Floricultor;
 
 @Controller
 public class FlorEsController {
+
+    @Autowired
+    private FloricultorRepository floricultorRepository;
 
     @GetMapping("/")
     public String index(Authentication authentication, Model model) {
@@ -18,7 +26,16 @@ public class FlorEsController {
             rol = authentication.getAuthorities().stream()
                     .map(GrantedAuthority::getAuthority)
                     .collect(Collectors.joining(","));
+            String email = authentication.getName();
+
+            Optional<Floricultor> floricultor = floricultorRepository.findByCorreoElectronico(email);
+
+            Floricultor floricultorObj = floricultor.orElse(new Floricultor()); // Devuelve un objeto vacío si no se encuentra
+            model.addAttribute("rol", rol);
+            model.addAttribute(floricultorObj);
+            return "index";  
         }
+
         model.addAttribute("rol", rol);
         return "index";  
     }
@@ -30,4 +47,3 @@ public class FlorEsController {
 }
 
 
- 
